feb67bf24448e7aa17c9e54dc2bebedde7ecd763
flowerwine <2914518090@qq.com>
2025-04-09 17:39:04 +0800

    first commit

---
+++ cxx/src/main/java/com/flowerwine/cxx/CxxApplication.java
@@ -0,0 +1,13 @@
+package com.flowerwine.cxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxxApplication.class, args);
+	}
+
+}
---
+++ cxx/src/test/java/com/flowerwine/cxx/CxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowerwine.cxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
